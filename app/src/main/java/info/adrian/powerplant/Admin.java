889bdb084460dd09efbad26d87662ee002eea685
bryhernandez <61296527+bryhernandez@users.noreply.github.com>
2021-04-21 21:34:59 -0700

    Created the Admin.java and the activity_admin.xml

---
+++ app/src/main/java/info/adrian/powerplant/Admin.java
@@ -0,0 +1,6 @@
+package info.adrian.powerplant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Admin extends AppCompatActivity {
+}
--- app/src/main/java/info/adrian/powerplant/LoginActivity.java
+++ app/src/main/java/info/adrian/powerplant/LoginActivity.java
@@ -1,21 +1,17 @@
 package info.adrian.powerplant;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
-import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseUser;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.parse.LogInCallback;
+import com.google.android.material.textfield.TextInputEditText;
+import com.parse.ParseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
