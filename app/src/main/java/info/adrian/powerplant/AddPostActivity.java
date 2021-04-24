e06eb57b1c66d7d0f49d39f200f6f833725df0ae
Pedro Muñoz <munoz@users.noreply.github.com>
2021-04-24 12:48:00 -0700

    Configured camera access. Post can be saved to Parse.

--- app/src/main/java/info/adrian/powerplant/AddPostActivity.java
+++ app/src/main/java/info/adrian/powerplant/AddPostActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -28,9 +29,9 @@ import com.parse.SaveCallback;
 import java.io.File;
 import java.util.List;
 
-public class AddPost extends AppCompatActivity {
+public class AddPostActivity extends AppCompatActivity {
 
-    public static final String TAG = "AddPost";
+    public static final String TAG = "AddPostActivity";
     public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private EditText etDescription;
     private Button captureImageButton;
@@ -79,38 +80,29 @@ public class AddPost extends AppCompatActivity {
     }
 
     private void launchCamera() {
+        Log.i("Camera","lauchCamer() entered");
         // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Log.i("Camera","85");
         // Create a File reference for future access
         photoFile = getPhotoFileUri(photoFileName);
+        Log.i("Camera","88");
 
         // wrap File object into a content provider
         // required for API >= 24
         // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(AddPost.this, "com.codepath.fileprovider", photoFile);
+        Uri fileProvider = FileProvider.getUriForFile(AddPostActivity.this, "info.adrian.fileprovider", photoFile);
+        Log.i("Camera","94");
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+        Log.i("Camera","96");
 
         // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
         // So as long as the result is not null, it's safe to use the intent.
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             // Start the image capture intent to take photo
+            Log.i("Camera","intent.resolveActivity(getPackageManager())");
             startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                // RESIZE BITMAP, see section below
-                // Load the taken image into a preview
-                postImage.setImageBitmap(takenImage);
-            } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
         }
     }
 
@@ -129,6 +121,23 @@ public class AddPost extends AppCompatActivity {
         // Return the file target for the photo based on filename
         return new File(mediaStorageDir.getPath() + File.separator + fileName);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                postImage.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
 
     private void savePost(String description, ParseUser currentUser, File photoFile) {
         Post post = new Post();
--- app/src/main/java/info/adrian/powerplant/FeedPageActivity.java
+++ app/src/main/java/info/adrian/powerplant/FeedPageActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,7 +25,7 @@ public class FeedPageActivity extends AppCompatActivity {
         addPostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent postActivity = new Intent(getApplicationContext(), AddPost.class);
+                Intent postActivity = new Intent(getApplicationContext(), AddPostActivity.class);
                 startActivity(postActivity);
             }
         });
