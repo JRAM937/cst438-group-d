5fac1edac8551bbfd1494a7bb59deb7ebd3a1183
Adrian Ortiz <30380357+AdrianOrtiz-ux@users.noreply.github.com>
2021-04-23 21:38:57 -0700

    added button, to go to add post activity

---
+++ app/src/main/java/info/adrian/powerplant/AddPost.java
@@ -0,0 +1,14 @@
+package info.adrian.powerplant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+    }
+}
--- app/src/main/java/info/adrian/powerplant/FeedPageActivity.java
+++ app/src/main/java/info/adrian/powerplant/FeedPageActivity.java
@@ -2,7 +2,10 @@ package info.adrian.powerplant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,5 +21,14 @@ public class FeedPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_page);
+
+        Button addPostButton = (Button) findViewById(R.id.add_post);
+        addPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent postActivity = new Intent(getApplicationContext(), AddPost.class);
+                startActivity(postActivity);
+            }
+        });
     }
 }
