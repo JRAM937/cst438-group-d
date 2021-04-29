4ccd09c65e9e7355112de26ff24ca3499b765f6a
Pedro Muñoz <munoz@users.noreply.github.com>
2021-04-28 23:16:41 -0700

    Connected Home icon -> Edit account Search icon -> Search activity (Commented out until search activity is created)

--- app/src/main/java/info/adrian/powerplant/FeedPageActivity.java
+++ app/src/main/java/info/adrian/powerplant/FeedPageActivity.java
@@ -1,10 +1,12 @@
 package info.adrian.powerplant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +14,9 @@ import android.widget.TextView;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import static info.adrian.powerplant.R.id.app_bar_home;
+import static info.adrian.powerplant.R.id.app_bar_search;
 
 public class FeedPageActivity extends AppCompatActivity {
 
@@ -38,6 +43,24 @@ public class FeedPageActivity extends AppCompatActivity {
                 startActivity(postActivity);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case app_bar_home:
+                Intent editAccountActivity = new Intent(getApplicationContext(), UserEditAccountActivity.class);
+                startActivity(editAccountActivity);
+                return true;
+
+//            case app_bar_search:
+//                Intent searchActivity = new Intent(getApplicationContext(), SearchActivity.class);
+//                startActivity(searchActivity);
+//                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
